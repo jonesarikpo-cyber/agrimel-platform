187a1ea4c98c251f03d1089dedd7bf7f3a575920
jonesarikpo-cyber <jonesarikpo@gmail.com>
2026-09-16 18:12:46 +0100

    feat: route live commerce report

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import Storefront from './Storefront'
+import CommerceReport from './CommerceReport'
 import './styles.css'
 
 const path=window.location.pathname.replace(/\/$/,'')
-const Root=path==='/store'?Storefront:App
+const Root=path==='/store'?Storefront:path==='/commerce-report'?CommerceReport:App
 createRoot(document.getElementById('root')!).render(<React.StrictMode><Root/></React.StrictMode>)
